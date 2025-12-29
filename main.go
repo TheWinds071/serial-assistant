714934f0b22459ac36c061b8d401341ffa322291
TheWinds <chen2011332114@outlook.com>
2025-12-29 16:39:51 +0800

    Align window background with main theme

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 242, G: 241, B: 237, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
